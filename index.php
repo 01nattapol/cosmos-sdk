e952f61d460d76244e4ea90e182fbd9a96f74155
nattapol <flybird.developer503@gmail.com>
2024-09-17 17:05:59 +0700

    Curl to fetch data from https protocol

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-// require 'vendor/autoload.php';
-// use GuzzleHttp\Client;
+require 'vendor/autoload.php';
+use GuzzleHttp\Client;
 
 $address = 'gea1zhl37fpn556f89fd43agnzmdj8wcs43h24e7mr';
 // $address = 'gea12jan6lldlpzs0cykq5lvczw2yasp6zxzaqefw5';
@@ -13,34 +13,19 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-// curl_setopt($ch, CURLOPT_CAINFO, "C:/xampp/php/extras/ssl/cacert.pem");
 $response = curl_exec($ch);
 
 if(curl_errno($ch)){
     echo 'clrl error: ' . curl_error($ch);
 } else{
+    $nodeInfo = json_decode($response, true);
     echo '<pre>';
-    print_r($response);
+    print_r($nodeInfo);
 }
-exit;
-
-
-$client = new Client();
-$response = $client->request('GET', $url);
-
-echo $response->getBody();
-exit;
 
 
 
-// $url = $brodcast_tx;
-// $url = 'https://gea.uatserver.biz:8387/cosmos/auth/v1beta1/accounts';
-// $url = 'https://gea.uatserver.biz:8387/cosmos/auth/v1beta1/accounts/'.$address;
-// $url = 'https://gea.uatserver.biz:8387/cosmos/auth/v1beta1/params';
-// $url = 'https://gea.uatserver.biz:8387/cosmos/authz/v1beta1/grants';
-$response = file_get_contents($url);
-$nodeInfo = json_decode($response, true);
-echo '<pre>';
-print_r($nodeInfo);
+
+
 
 
